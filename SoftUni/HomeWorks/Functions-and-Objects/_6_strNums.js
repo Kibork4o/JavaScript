d44262e71433d524e6d9f10ae45b9bb64f066929
Kibork4o <kibork4o@gmail.com>
2014-07-21 19:37:35 +0300

    Create _6_strNums.js
    Function that sum two integers

---
+++ SoftUni/HomeWorks/Functions-and-Objects/_6_strNums.js
@@ -0,0 +1,11 @@
+function twoStringNums(arr){
+    var num1=arr[0];
+    var num2=arr[1];
+    var string=num1 + " + " + num2;
+    var result=eval(string);
+    return eval(result);
+}
+console.log(['123456789', '123456789']);
+console.log(['887987345974539','4582796427862587']);
+console.log(twoStringNums(['347135713985789531798031509832579382573195807',
+        '817651358763158761358796358971685973163314321']));
