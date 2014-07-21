6ab8faa2014559d916253e1898e846ebc3ee7fcc
Kibork4o <kibork4o@gmail.com>
2014-07-21 20:33:06 +0300

    Update _6_strNums.js

--- SoftUni/HomeWorks/Functions-and-Objects/_6_strNums.js
+++ SoftUni/HomeWorks/Functions-and-Objects/_6_strNums.js
@@ -5,7 +5,7 @@ function twoStringNums(arr){
     var result=eval(string);
     return eval(result);
 }
-console.log(['123456789', '123456789']);
-console.log(['887987345974539','4582796427862587']);
+console.log(twoStringNums(['123456789', '123456789']));
+console.log(twoStringNums(['887987345974539','4582796427862587']));
 console.log(twoStringNums(['347135713985789531798031509832579382573195807',
         '817651358763158761358796358971685973163314321']));
